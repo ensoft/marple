69c6084c6697ab405e619889b6394a59852f8122
franznowak <fgn24@cam.ac.uk>
2018-06-26 11:46:05 +0100

    move functions from sched to perf

---
+++ src/Interface/perf.py
@@ -0,0 +1,33 @@
+from subprocess import call
+
+
+def collect(t):
+    """
+    Collect system data using perf
+    :param t:
+        time in seconds for which to collect the data
+
+    """
+    call(["perf", "record",  "sleep", str(t)])
+
+
+def collect_sched(t):
+    """
+        Collect CPU scheduling data using perf sched
+        :param t:
+            time in seconds for which to collect the data
+
+    """
+    call(["perf", "sched", "record", "sleep", str(t)])
+
+
+def map_sched():
+    """
+            Display the collected scheduling data as a map.
+
+            Each columns represents a CPU core, each entry is
+            a process whose name can be found in the legend
+            on the right.
+
+    """
+    call(["perf", "sched", "map"])
