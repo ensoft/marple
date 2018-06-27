de960236e7145be8edd5442ac8e95a097bda293e
franznowak <fgn24@cam.ac.uk>
2018-06-27 14:35:40 +0100

    Remove frequency parameter from collection function

--- src/sched.py
+++ src/sched.py
@@ -1,16 +1,14 @@
 from .interface import perf
 
 
-def collect(t = 10, f = 100):
+def collect(t=10):
     """
     Uses perf module to collect scheduling data
 
     :param t:
         time in seconds for the data to be collected
 
-    :param f:
-        frequency of taking samples in Hz
 
     """
-    perf.collect_sched_all(t, f)
+    perf.collect_sched_all(t)
 
