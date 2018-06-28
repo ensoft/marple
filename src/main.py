fa88574a2135ae276fff8627a8250919f4485165
franznowak <fgn24@cam.ac.uk>
2018-06-28 11:41:50 +0100

    Explain main call

--- src/main.py
+++ src/main.py
@@ -2,4 +2,6 @@ import sys
 from src.modules import controller
 
 if __name__ == "__main__":
+    # Call main function with command line arguments
+    # Excluding argv[0] (program name)
     controller.main(sys.argv[1:])
