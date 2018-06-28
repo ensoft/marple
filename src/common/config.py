7ec0222d3e0fad7459f02b8b295aabacee120fc5
franznowak <fgn24@cam.ac.uk>
2018-06-28 12:26:49 +0100

    Add default frequency.

--- src/common/config.py
+++ src/common/config.py
@@ -1,10 +1,14 @@
 # Constants
+
 blocking = False
+frequency = 99
+# The default frequency is 99 rather than 100 to avoid
+# recording in lockstep with some periodic activity.
 
 
 def is_blocking():
     """
-    Tell the caller if it should block waiting for the data collection to finish
+    Return config value defining whether caller should block waiting for the data collection to finish
 
     The calling module function, e.g. perf.collect can either return while data is still being collected
     or wait for the collection to finish. This function is called to tell it which of these it should do.
@@ -13,3 +17,14 @@ def is_blocking():
         A boolean value that specifies whether to block
     """
     return blocking
+
+
+def get_default_frequency():
+    """
+        Return the default frequency
+
+        :return:
+            The default frequency in Hertz
+
+        """
+    return frequency
