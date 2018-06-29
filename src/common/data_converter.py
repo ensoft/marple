cc5b4bf95f35f9d37011fdded60b43d4943cd9be
franznowak <fgn24@cam.ac.uk>
2018-06-29 12:36:41 +0100

    Add function stubs for conversion

---
+++ src/common/data_converter.py
@@ -0,0 +1,24 @@
+"""
+Converts data created by lower level modules
+to a format that can be used by displaying tools
+"""
+
+
+def to_g2():
+    """
+    Convert data to CPEL format for g2
+
+    Takes an input file and a format and creates a file readable by the vpp/g2 program
+    
+    """
+    # Stub
+    pass
+
+
+def to_flame():
+    """
+    Convert data into the format required by Brendan Greggs flame graph tool
+
+    """
+    # Stub
+    pass
