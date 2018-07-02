e8e45eedd95a1c99aaac65f09a9a85a73d275c97
franznowak <fgn24@cam.ac.uk>
2018-07-02 10:56:30 +0100

    Add comments and documentation, eliminate unnecessary state

--- src/common/file.py
+++ src/common/file.py
@@ -1,7 +1,22 @@
+# -------------------------------------------------------------
+# file.py - generates unique file names
+# June-July 2018 - Franz Nowak
+# -------------------------------------------------------------
+
+"""
+Generates unique file names
+
+Provides functions to generate a filename if no name was provided by the user.
+
+"""
+
+__all__ = ["create_name"]
+
 import uuid
 
 
 def create_name():
     """Create a new unique generic name for a file"""
-    name = str(uuid.uuid4())
-    return name
+    return str(uuid.uuid4())
+
+# Add: provide a prefix for the name, timestamp, etc.
