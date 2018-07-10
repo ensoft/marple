49baea06aa56969227d2be6ed648d4c4ab96f238
franznowak <fgn24@cam.ac.uk>
2018-07-10 11:21:09 +0100

    Change name from leap to marple

--- Readme.md
+++ Readme.md
@@ -1,5 +1,4 @@
-leap -
-Linux Analyse and Enhance performance
+MARPLE - Multi-tool Analysis of Runtime Performance on Linux Environments
 
 Note:
 Needs to be executed with root priviliges for using the linux kernel profiling tools.
--- main.py
+++ main.py
@@ -21,14 +21,14 @@ import common.output as output
 logger = logging.getLogger('main')
 logger.setLevel(logging.DEBUG)
 
-DIRECTORY = "/var/log/leap/"
+DIRECTORY = "/var/log/marple/"
 
 # Check whether user is root, otherwise exit
 if os.geteuid() != 0:
-    exit("Error: You need to have root privileges to run leap.")
+    exit("Error: You need to have root privileges to run marple.")
 
 try:
-    # Check if leap logging directory exists, else create one
+    # Check if marple logging directory exists, else create one
     if not os.path.exists(DIRECTORY):
         os.makedirs(DIRECTORY)
 
@@ -37,10 +37,11 @@ try:
     logging.basicConfig(format="%(asctime)s %(name)-12s "
                                "%(levelname)-8s %(message)s",
                         datefmt="%m-%d %H:%M",
-                        filename=DIRECTORY + "leap" + str(os.getpid()) + ".log",
+                        filename=DIRECTORY + "marple" + str(os.getpid()) +
+                                ".log",
                         filemode="w")
 
-    # use leap log across the whole module
+    # use marple log across the whole module
     logger = logging.getLogger('main')
     logger.setLevel(logging.DEBUG)
 
@@ -57,17 +58,17 @@ if __name__ == "__main__":
     logger.info("Application started.")
     try:
         # Call main function with command line arguments
-        # Excluding argv[0] (program name: leap)
+        # Excluding argv[0] (program name: marple)
         # and argv[1] (function name: {collect,display})
         if len(sys.argv) < 2:
-            output.print_("usage: leap COMMAND\n The COMMAND "
+            output.print_("usage: marple COMMAND\n The COMMAND "
                           "can be either \"collect\" or \"display\"")
         elif sys.argv[1] == "collect":
             collect_controller.main(sys.argv[2:])
         elif sys.argv[1] == "display":
             display_controller.main(sys.argv[2:])
         else:
-            output.print_("usage: leap COMMAND\n The COMMAND "
+            output.print_("usage: marple COMMAND\n The COMMAND "
                           "can be either \"collect\" or \"display\"")
     except Exception as ex:
         # If anything goes wrong, handle it here
