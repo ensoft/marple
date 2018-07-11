bfb0ccd29ba9c999862302dff8c4fe484e433293
franznowak <fgn24@cam.ac.uk>
2018-07-11 10:44:09 +0100

    Refactor to data_types and add typing hints for the named tuple SchedEvent

---
+++ collect/converter/data_types.py
@@ -0,0 +1,20 @@
+# -------------------------------------------------------------
+# data_types.py - intermediate form of data to be converted
+# June-July 2018 - Franz Nowak
+# -------------------------------------------------------------
+
+"""Data types for intermediate formats used in later data processing."""
+
+from typing import NamedTuple
+
+
+class SchedEvent(NamedTuple):
+    """Represents a single scheduler event."""
+    name: str
+    pid: int
+    cpu: int
+    time: str
+    type: str
+
+
+
