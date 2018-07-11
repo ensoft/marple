fa4a2dcdf1ad4bc76218aa4afa0f611c6f049234
franznowak <fgn24@cam.ac.uk>
2018-07-11 12:41:30 +0100

    Add generator argument to converter functions and finish structure.

--- collect/converter/main.py
+++ collect/converter/main.py
@@ -10,25 +10,41 @@ Gets the data that was collected by an interface module and converted into
 formatted objects and writes them into a file that was provided by the user.
 
 """
+from collections import Counter
 
 
-def create_stack_data(filename):
+def create_stack_data(generator, filename):
     """
-    Save the stack data from the generator into a file.
+    Count, sort and saves the stack data from the generator into a file.
 
+    :param generator:
+        The generator of stack objects
     :param filename:
         The name of the file into which to store the output.
 
     """
+    # Count stack occurrences
+    cnt = Counter()
+    for line in generator:
+        cnt[line] += 1
+
+    # @Sort by keys (recursively by ascending index)
+    pass
+
+    # @Write to file
     pass
 
 
-def create_cpu_event_data(filename):
+def create_cpu_event_data(generator, filename):
     """
-    Save the event data from the generator into a file.
+    Saves the event data from the generator into a file.
 
+    :param generator:
+        The generator of SchedEvent objects
     :param filename:
         The name of the file into which to store the output.
 
     """
+
+    # @Write to file
     pass
