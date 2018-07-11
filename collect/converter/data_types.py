cbd1c7896c9a5db83b293ddde6454253bfe32131
franznowak <fgn24@cam.ac.uk>
2018-07-11 10:58:09 +0100

    convert cpu attribute from string to int and add error handling.

--- collect/converter/data_types.py
+++ collect/converter/data_types.py
@@ -4,17 +4,31 @@
 # -------------------------------------------------------------
 
 """Data types for intermediate formats used in later data processing."""
-
+import re
 from typing import NamedTuple
+
+from common import output
 
 
 class SchedEvent(NamedTuple):
     """Represents a single scheduler event."""
     name: str
     pid: int
-    cpu: int
+    cpu: str
     time: str
     type: str
 
+    def __init__(self):
+        # strip off brackets (for perf output)
+        if re.match("\[\d+\]", self.cpu) is not None:
+            re.sub("\[", "", self.cpu)
+            re.sub("\]", "", self.cpu)
 
-
+        # convert to int
+        try:
+            self.cpu = int(self.cpu)
+        except ValueError as verr:
+            output.error("Unexpected formatting error. "
+                         "Please refer to log for details.",
+                         "Wrong format detected, cpu in sched event "
+                         "is not a number")
