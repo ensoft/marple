b79c7a4d3493ac674bcf25e78c7ed140619016d4
franznowak <fgn24@cam.ac.uk>
2018-07-11 12:02:35 +0100

    Refactor rename the converter main file, add docstrings.

--- collect/converter/converter.py
+++
@@ -1,4 +0,0 @@
-
-def create_stack_data(filename):
-    """Get the relevant stack tracing data"""
-    pass
---
+++ collect/converter/main.py
@@ -0,0 +1,34 @@
+# -------------------------------------------------------------
+# converter/main.py - Saves data objects into a file.
+# June-July 2018 - Franz Nowak
+# -------------------------------------------------------------
+
+"""
+Saves data objects into a file.
+
+Gets the data that was collected by an interface module and converted into
+formatted objects and writes them into a file that was provided by the user.
+
+"""
+
+
+def create_stack_data(filename):
+    """
+    Save the stack data from the generator into a file.
+
+    :param filename:
+        The name of the file into which to store the output.
+
+    """
+    pass
+
+
+def create_event_data(filename):
+    """
+    Save the event data from the generator into a file.
+
+    :param filename:
+        The name of the file into which to store the output.
+
+    """
+    pass
