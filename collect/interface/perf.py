c1cdbca8cb4c1efed85d64cdec62589a10ae622a
franznowak <fgn24@cam.ac.uk>
2018-07-16 12:30:53 +0100

    Markups: Convert perf to stack (parse) function in perf into a StackParser class.

--- collect/interface/perf.py
+++ collect/interface/perf.py
@@ -78,7 +78,8 @@ def get_stack_data():
         outfile.write(sp.stdout.read().decode())
         logger.error(sp.stderr.read().decode())
 
-    return _stack_collapse(filename)
+    sp = StackParser(filename)
+    return sp.stack_collapse()
 
 
 def get_sched_data():
@@ -95,7 +96,7 @@ def get_sched_data():
     filename = file.create_unique_temp_filename()
 
     sp = subprocess.Popen(["perf", "sched", "script", "-F",
-                          "comm,pid,cpu,time,event"],
+                           "comm,pid,cpu,time,event"],
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE)
 
@@ -141,7 +142,7 @@ def _sched_data_gen(filename):
             if match is None:
                 logger.debug("Failed to parse event data: {} Expected "
                              "format: name pid cpu time event".format(
-                              event_data))
+                    event_data))
                 continue
 
             event = SchedEvent(name=match.group("name"),
@@ -156,148 +157,218 @@ def _sched_data_gen(filename):
     os.remove(filename)
 
 
-def _stack_collapse(filename):
+class StackParser:
     """
     Goes through input line by line to fold the stacks.
 
-    This function is a Python adaptation of Brendan Gregg's Perl script
-    stackcollapse-perf.pl from his flamegraph repository.
-    It differs in that it has fewer options and returns a generator to get
-    single stacks lazily and without counting them.
-    This is in order to integrate it with the marple program structure.
+    Takes stacks that were captured by perf and converts them to stack objects.
 
-    :param filename:
-        A text file or similar input containing lines of stack data
-        output from perf.
-
-    :return:
-        A generator of lists that contain information about a single stack.
     """
+    # ---------------------------------------------------------
+    # Regular expressions to match lines of the perf output:
+
+    _emptyline = r"$^"
+    # Matches an empty line
+
+    _baseline = r"(?P<comm>\S.+?)\s+(?P<pid>\d+)/*(?P<tid>\d+)*\s+"
+    # Matches "perf script" output, first line of a stack (baseline)
+    # eg. "java 25607 4794564.109216: cycles:"
+    # or  "V8 WorkerThread 24636/25607 [002] 6544038.708352: cpu-clock:"
+
+    _eventtype = r"(?P<event>\S+):\s*$"
+    # Matches the event type of the stack, found at the end of the baseline
+    #   e.g. cycles:ppp:
+
+    _stackline = r"\s*(?P<pc>\w+)\s*(?P<rawfunc>.+)\((?P<mod>\S*)\)"
+    # Matches the other lines of a stack, i.e. the ones above the base.
+    # e.g ffffffffabe0c31d intel_pmu_enable_ ([kernel.kallsyms])
+
+    _symbol_offset = r"\+0x[\da-f]+$"
+    # Matches symbol offset
+    # eg in: 7fffb84c9afc cpu_startup_entry+0x800047c022ec ([kernel.kallsyms])
+
+    # --------------------------------------------------------
+
+    # The cached stack data
     stack = []
-    pid = None
-    comm = None
+    # The process name
     pname = None
+    # Variables to handle filtering for only one event
     event_filter = ""
     event_defaulted = False
     event_warning = False
 
-    logger.info("Starting to convert perf output to stacks")
-    with(open(filename, "r")) as input_:
-        for line in input_:
-            # If end of stack, save cached data.
-            if re.match("^$", line) is not None:
-                # ignore filtered samples
-                if pname is None:
-                    continue
-                stack.insert(0, pname)
-                if stack:
-                    yield tuple(stack)
-                stack = []
-                pname = None
-                continue
+    def __init__(self, filename):
+        """ Initialises the Parser.
 
-            #
-            # event record start
-            #
-            if re.match("(\S.+?)\s+(\d+)/*(\d+)*\s+", line) is not None:
-                # Matches "perf script" output, first line of a stack (baseline)
-                # eg, "java 25607 4794564.109216: cycles:"
-                # eg, "java 12688 [002] 6544038.708352: cpu-clock:"
-                # eg, "V8 WorkerThread 25607 4794564.109216: cycles:"
-                # eg, "java 24636/25607 [000] 4794564.109216: cycles:"
-                # eg, "java 12688/12764 6544038.708352: cpu-clock:"
-                # eg, "V8 WorkerThread 24636/25607 [000] 94564.109216: cycles:"
-                # other combinations possible
-                m = re.match("(\S.+?)\s+(\d+)/*(\d+)*\s+", line)
-                try:
-                    (comm, pid, tid) = m.group(1), m.group(2), m.group(3)
-                except IndexError:
-                    # No tid found
-                    tid = pid
-                    pid = "?"
-                m = re.search("(\S+):\s*$", line)
-                if m is not None:
-                    # By default only show events of the first encountered
-                    # event type. Merging together different types, such as
-                    # instructions and cycles, produces misleading results.
-                    event = m.group(1)
-                    if event_filter == "":
-                        # By default only show events of the first
-                        # encountered event type. Merging together different
-                        # types, such as instructions and cycles, produces
-                        # misleading results.
-                        event_filter = event
-                        event_defaulted = True
-                    elif event != event_filter:
-                        if event_defaulted and not event_warning:
-                            # only print this warning if necessary:
-                            # when we defaulted and there was
-                            # multiple event types.
-                            logger.error("Filtering for events of type {}"
-                                         .format(event_filter))
-                            event_warning = True
-                        continue
+        :param filename:
+            A text file or similar input containing lines of stack data
+            output from perf.
 
-                (m_pid, m_tid) = pid, tid
+        """
 
-                if INCLUDE_TID:
-                    pname = "{}-{}/{}".format(comm, m_pid, m_tid)
-                elif INCLUDE_PID:
-                    pname = "{}-{}".format(comm, m_pid)
+        self.filename = filename
+
+    def _line_is_empty(self, line):
+        """Checks whether line is an empty line."""
+
+        return re.match(self._emptyline, line) is not None
+
+    def _line_is_baseline(self, line):
+        """Checks whether a line is a stack baseline."""
+
+        return re.match(self._baseline, line) is not None
+
+    def _line_is_stackline(self, line):
+        """Checks whether a line is a stack line that is not a baseline."""
+
+        return re.match(self._stackline, line) is not None
+
+    def _make_stack(self):
+        """Creates a stack tuple from cached data and yields it."""
+
+        # if there is no pname, we have not processed one yet, which means
+        #   that past ones have been filtered
+        if self.pname is None:
+            return
+
+        # Finish making the stack and yield it
+        self.stack.insert(0, self.pname)
+        yield tuple(self.stack)
+
+        # Reset the cache
+        self.stack = []
+        self.pname = None
+
+    def _parse_baseline(self, line):
+        """Matches a stack baseline and extracts its info."""
+
+        m = re.match(self._baseline, line)
+        # eg. "java 25607 4794564.109216: cycles:"
+
+        (comm, pid) = m.group("comm"), m.group("pid")
+
+        try:
+            tid = m.group("tid")
+
+        except IndexError:
+            # No tid found (only third group is optional)
+            tid = pid
+            pid = "?"
+
+        if re.search(self._eventtype, line):
+            # Matches the event type of the stack, found at the
+            #   end of the baseline
+            # e.g. cycles:ppp:
+
+            m = re.search(self._eventtype, line)
+            # By default only show events of the first encountered
+            #   event type. Merging together different types,
+            #   such as instructions and cycles, produces
+            #   misleading results.
+            event = m.group(1)
+
+            if self.event_filter == "":
+                self.event_defaulted = True
+
+            elif event != self.event_filter:
+                if self.event_defaulted and not self.event_warning:
+                    # only print this warning if necessary:
+                    # when we defaulted and there were
+                    # multiple event types.
+                    logger.error("Filtering for events of type {}"
+                                 .format(self.event_filter))
+                    self.event_warning = True
+                return
+
+        if INCLUDE_TID:
+            self.pname = "{}-{}/{}".format(comm, pid, tid)
+        elif INCLUDE_PID:
+            self.pname = "{}-{}".format(comm, pid)
+        else:
+            self.pname = comm
+        # replace space with underscore
+        self.pname = re.sub("\s", "_", self.pname)
+
+    def _parse_stackline(self, line):
+        """Matches a stack line that is not a baseline and extracts its info."""
+
+        m = re.match(self._stackline, line)
+        # e.g ffffffffabe0c31d intel_pmu_enable_ ([kernel.kallsyms])
+
+        # Ignore filtered samples
+        if self.pname is None:
+            return
+
+        pc, rawfunc, mod = m.group("pc"), m.group("rawfunc").rstrip(), \
+                           m.group("mod")
+
+        # Linux 4.8 includes symbol offsets in perf script output,
+        # eg 7fffb84c9afc cpu_startup_entry+0x800047c022ec
+        #   ([kernel.kallsyms])
+
+        # strip these off:
+        rawfunc = re.sub(self._symbol_offset, "", rawfunc)
+
+        # Original perl script adds inline here if required by user
+
+        # Skip process names
+        if re.match("\(", rawfunc):
+            return
+
+        # Not sure what inlining is for...
+        inline = ""
+        for func in re.split("->", rawfunc):
+            if func == "[unknown]":
+                # use module name instead, if known
+                if mod != "[unknown]":
+                    func = mod
+                    func = re.sub(".*/", "", func)
+
+            # TODO: Tidy generic stuff
+
+            if len(inline) > 0:
+                # Mark as inlined
+                func += "_[i]"
+
+            inline += func
+
+        self.stack.insert(0, inline)
+
+    def stack_collapse(self):
+        """
+        Goes through input line by line to fold the stacks.
+
+        Calls the appropriate function to parse each line.
+        Returns a generator to get single stacks lazily.
+
+        :return:
+            An iterable of tuples that contain information about stacks.
+
+        """
+
+        logger.info("Starting to convert perf output to stacks")
+        with(open(self.filename, "r")) as input_:
+
+            for line in input_:
+
+                # If end of stack, save cached data.
+                if self._line_is_empty(line):
+                    # Matches empty line
+                    self._make_stack()
+
+                # event record start
+                elif self._line_is_baseline(line):
+                    # Matches "perf script" output, first line of a stack
+                    self._parse_baseline(line)
+
+                # stack line
+                elif self._line_is_stackline(line):
+                    # Matches the other lines of a stack above the baseline
+                    self._parse_stackline(line)
+
+                # if nothing matches, log an error
                 else:
-                    pname = comm
+                    logger.error("Unrecognized line: {}".format(line))
 
-                # original perl script has transliteration here:
-                # $pname =~ tr/ /_/;
-
-            #
-            # stack line
-            #
-            elif re.match("\s*(\w+)\s*(.+)\((\S*)\)", line) is not None:
-                # Matches the other lines of a stack,
-                # i.e. the ones above the base.
-                # e.g ffffffffabe0c31d _intel_pmu_enable_ ([kernel.kallsyms])
-                m = re.match("\s*(\w+)\s*(.+)\((\S*)\)", line)
-
-                # ignore filtered samples
-                if pname is None:
-                    continue
-
-                pc, rawfunc, mod = m.group(1), m.group(2).rstrip(), m.group(3)
-
-                # Linux 4.8 included symbol offsets in perf script output by
-                # default, eg:
-                # 7fffb84c9afc cpu_startup_entry+0x800047c022ec
-                # ([kernel.kallsyms])
-                # strip these off:
-                rawfunc = re.sub("\+0x[\da-f]+$", "", rawfunc)
-
-                # Original perl script adds inline here if required by user
-
-                # skip process names
-                if re.match("\(", rawfunc):
-                    continue
-
-                inline = ""
-                for func in re.split("->", rawfunc):
-                    if func == "[unknown]":
-                        # use module name instead, if known
-                        if mod != "[unknown]":
-                            func = mod
-                            func = re.sub(".*/", "", func)
-
-                        # Original perl script has option to include address.
-                    # Original perl script has some optional tidying up here for
-                    # Java and other generic stuff
-
-                    if len(inline) > 0:
-                        # Mark as inlined
-                        func += "_[i]"
-
-                    inline += func
-
-                stack.insert(0, inline)
-
-            else:
-                logger.error("Unrecognized line: {}".format(line))
-    logger.info("Conversion to stacks successful")
+        logger.info("Conversion to stacks successful")
