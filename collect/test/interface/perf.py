f8c0ef4febe3d61084ba2755b21a6b8757455fc8
franznowak <fgn24@cam.ac.uk>
2018-07-18 12:36:08 +0100

    Finish perf StackParser test.

--- collect/test/interface/perf.py
+++ collect/test/interface/perf.py
@@ -1,5 +1,7 @@
 import os
 
+from unittest.mock import patch
+
 import collect.interface.perf as perf
 import collect.converter.datatypes as datatypes
 import collect.test.util as util
@@ -24,8 +26,16 @@ class _BaseTest(util.BaseTest):
 
 
 class _StackParserTest(_BaseTest):
-    """Base test class for testing the stack parser class"""
-    stack_parser = perf.StackParser("")
+    """Base test class for testing the stack parser class."""
+    def setUp(self):
+        """Creates a generic empty StackParser object."""
+        super().setUp()
+        self.stack_parser = perf.StackParser("")
+
+    def tearDown(self):
+        """Deallocates the StackParser objec.t"""
+        super().tearDown()
+        self.stack_parser = None
 
 
 # -----------------------------------------------------------------------------
@@ -36,11 +46,11 @@ class DataGenTest(_BaseTest):
     """Test class for the _sched_data_get() generator."""
 
     def setUp(self):
-        """Per-test set-up"""
+        """Per-test set-up."""
         super().setUp()
 
     def tearDown(self):
-        """Per-test tear-down"""
+        """Per-test tear-down."""
         super().tearDown()
 
     def test_basic(self):
@@ -86,11 +96,11 @@ class StackParserTest(_StackParserTest):
     """Test class for the StackParser class."""
 
     def setUp(self):
-        """Per-test set-up"""
+        """Per-test set-up."""
         super().setUp()
 
     def tearDown(self):
-        """Per-test tear-down"""
+        """Per-test tear-down."""
         super().tearDown()
 
     def test_is_empty(self):
@@ -102,7 +112,7 @@ class StackParserTest(_StackParserTest):
         self.assertFalse(self.stack_parser._line_is_empty(" Not empty "))
 
     def test_is_baseline(self):
-        """Tests the function recognising a baseline"""
+        """Tests the function recognising a baseline."""
         # Variations of baselines
         baselines = ["java 25607 4794564.109216: cycles:",
                      "java 12688 [002] 6544038.708352: cpu-clock:",
@@ -114,7 +124,7 @@ class StackParserTest(_StackParserTest):
             self.assertTrue(self.stack_parser._line_is_baseline(line))
 
     def test_is_stackline(self):
-        """Tests the function recognising a stackline"""
+        """Tests the function recognising a stackline."""
         # Variations of stacklines
         stacklines = \
             ["	ffffffffa01bbd11 __perf_event_enable ([kernel.kallsyms])",
@@ -126,19 +136,90 @@ class StackParserTest(_StackParserTest):
         for line in stacklines:
             self.assertTrue(self.stack_parser._line_is_stackline(line))
 
-    def test_stack_collapse_basic(self):
-        """Check the stack_collapse function calls the appropriate parsers"""
-        # can stub out the is_... functions as they are tested separately
-        pass
+    def test_parse_baseline_pname_only(self):
+        """Tests the extraction of process names in _parse_baseline."""
+        # Check that a simple process name is extracted correctly
+        self.stack_parser._parse_baseline(line="java 27 464.116: cycles:")
+        self.assertTrue(self.stack_parser._pname == "java")
 
-    def test_parse_baseline(self):
-        pass
+        # Check that a space separated process name gets space replaced by _
+        self.stack_parser._parse_baseline(line="V8 WorkerThread 27 464.116: "
+                                               "cycles:")
+        self.assertTrue(self.stack_parser._pname == "V8_WorkerThread")
+
+        # Check that the event_filter defaulted:
+        self.assertTrue(self.stack_parser.event_filter == "cycles")
+
+    @patch("collect.interface.perf.INCLUDE_PID", True)
+    def test_parse_baseline_pid(self):
+        """Tests creation of pname with pid."""
+        self.stack_parser._parse_baseline(line="java 27 464.116: cycles:")
+        self.assertTrue(self.stack_parser._pname == "java-27")
+
+    @patch("collect.interface.perf.INCLUDE_TID", True)
+    def test_parse_baseline_tid(self):
+        """Tests creation of pname with pid and tid."""
+        self.stack_parser._parse_baseline(line="java 27/1 464.116: cycles:")
+        self.assertTrue(self.stack_parser._pname == "java-27/1")
 
     def test_parse_stackline(self):
-        pass
+        self.stack_parser._pname = "some_process"
+        self.stack_parser._parse_stackline(line="ffffffffabe0c31d "
+                                                "intel_pmu_enable_ (["
+                                                "kernel.kallsyms])")
+
+        self.assertTrue(self.stack_parser._stack == ["intel_pmu_enable_"])
 
     def test_make_stack(self):
-        pass
+        """Tests that a stack is correctly created from lines list and pname."""
+        # Example data
+        pname = "some_process"
+        stacklines = ["call1", "call2"]
+
+        # Expected output
+        expected = tuple([pname] + stacklines)
+
+        # Pass example data to stack parser
+        self.stack_parser._pname = pname
+        self.stack_parser._stack = stacklines
+
+        # Run the stack maker function
+        stackfolded = self.stack_parser._make_stack()
+
+        self.assertTrue(stackfolded == expected)
 
     def test_StackParser_complete(self):
-        pass
+
+        # Example of a single stack from perf extracted stack data
+        sample_stack = \
+            ["swapper     0 [003] 687886.672908:  108724462 cycles:ppp:\n" 
+             "ffffffffa099768b intel_idle ([kernel.kallsyms])\n"
+             "ffffffffa07e5ce4 cpuidle_enter_state ([kernel.kallsyms])\n"
+             "ffffffffa07e5f97 cpuidle_enter ([kernel.kallsyms])\n"
+             "ffffffffa00d299c do_idle ([kernel.kallsyms])\n"
+             "ffffffffa00d2723 call_cpuidle ([kernel.kallsyms])\n"
+             "ffffffffa00d2be3 cpu_startup_entry ([kernel.kallsyms])\n"
+             "ffffffffa00581cb start_secondary ([kernel.kallsyms])\n"
+             "ffffffffa00000d5 secondary_startup_64 ([kernel.kallsyms])\n"
+             "\n"]
+
+        # Expected output data
+        expected = [datatypes.StackEvent(stack=(
+            "swapper", "secondary_startup_64", "start_secondary",
+            "cpu_startup_entry", "call_cpuidle", "do_idle", "cpuidle_enter",
+            "cpuidle_enter_state", "intel_idle"))]
+
+        filename = self._TEST_DIR + "stack_collapse_test"
+
+        # Create a file with formatted data
+        with open(filename, "w") as file_:
+            file_.writelines(sample_stack)
+
+        self.stack_parser.filename = filename
+
+        # Run the whole stack_collapse function
+        events = self.stack_parser.stack_collapse()
+
+        import pdb;pdb.set_trace()
+
+        self.assertTrue(list(events) == expected)
