4837b6f0b789aa28f0fa68658a6a5fdf048b7574
Andrei Diaconu <acdiaconu@gmail.com>
2018-07-30 09:34:13 +0100

    Added an exception for not supported interfaces

--- common/exceptions.py
+++ common/exceptions.py
@@ -1,6 +1,7 @@
 # -------------------------------------------------------------
 # exceptions.py - custom exceptions
 # June-July 2018 - Franz Nowak
+# July 2018 - Andrei Diaconu
 # -------------------------------------------------------------
 
 """Custom exceptions"""
@@ -8,3 +9,9 @@
 
 class AbortedException(Exception):
     """Exception for when the program is aborted by decision of the user"""
+
+class NotSupportedException(Exception):
+    """Exception for when an interface is not suppported by the target kernel"""
+    def __init__(self, message, required_kernel):
+        super.__init__(message)
+        self.required_kernel = required_kernel
