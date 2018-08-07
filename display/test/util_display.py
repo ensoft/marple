7cb0122b337c0727cea7ae75af39ef1752047bb2
AndreiDiaconu1 <acdiaconu98@gmail.com>
2018-08-07 12:32:53 +0100

    Addde testing for display

---
+++ display/test/util_display.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------------------------
+# util_display.py - Test utilities
+#
+# July 2018, Matt Ware
+#
+# -----------------------------------------------------------------------------
+
+"""Test utilities."""
+
+
+import os
+import shutil
+import unittest
+
+
+__all__ = (
+    "BaseTest",
+)
+
+
+class BaseTest(unittest.TestCase):
+    _TEST_DIR = "/tmp/marple-test/"
+
+    def setUp(self):
+        """Per-test set-up"""
+        os.makedirs(self._TEST_DIR, exist_ok=True)
+
+    def tearDown(self):
+        """Per-test tear-down"""
+        shutil.rmtree(self._TEST_DIR)
