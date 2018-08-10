852ff4e19d0c15ce0f87cef8e3b93d7d7b6d61eb
franznowak <fgn24@cam.ac.uk>
2018-08-10 17:43:08 +0100

    Make stackplot show a csv file

--- display/stackplot.py
+++ display/stackplot.py
@@ -1,7 +1,16 @@
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot
+import io
+import matplotlib.pyplot as plt
 
 
-def show(mem_datapoints):
-    X, Y, Z = list(mem_datapoints)
-
+def show(csv):
+    df = pd.read_csv(io.StringIO(csv.strip()))
+    c = np.unique(df.C)
+    labels = ['{}'.format(_c) for _c in c]
+    args = [df[df.C == _c].B for _c in c]
+    plt.stackplot(np.unique(df.A), args, labels=labels)
+    plt.xlabel('A')
+    plt.ylabel('C')
+    plt.legend(loc='upper left')
+    plt.show()
