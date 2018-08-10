1808dbd77d07ff3ee60ef9ce55b7d0cc92e21a2d
franznowak <fgn24@cam.ac.uk>
2018-08-10 17:40:18 +0100

    Make smem return Generator and fill in empty datapoints

--- collect/interface/smem.py
+++ collect/interface/smem.py
@@ -1,14 +1,19 @@
 import re
 import subprocess
+import time
+
+from collect.interface.collecter import Collecter
+from common import datatypes
 
 
-def collect_memorydata(time: int, frequency: int, mode="name", number=5):
+# class MemoryGraph(Collecter):
+
+
+def collect_memory_data(time_in_seconds: int, mode="name", number=5):
     """
 
-    :param time:
+    :param time_in_seconds:
         The number of seconds for which to collect data.
-    :param frequency:
-        The frequency in Hz with which to sample the data.
     :param mode:
         "name" or "pid" or "command", to decide the labelling
     :param number:
@@ -18,11 +23,18 @@ def collect_memorydata(time: int, frequency: int, mode="name", number=5):
         An iterator of :class: `DataPoint` objects.
 
     """
-    ticks = time * frequency
-    datapoints = {}
 
-    for tick in range(ticks):
-        _time = tick/frequency
+    # Dict for the datapoints to be collected
+    datapoints = {}
+    
+    # Set to collect all labels that have been encountered
+    labels = set()
+
+    # Set the start time
+    start_time = time.monotonic()
+    current_time = 0.0
+
+    while current_time < time_in_seconds:
         if mode == "name":
             out = subprocess.getoutput("smem -c \"name pss\" | tac")
         elif mode == "command":
@@ -30,7 +42,7 @@ def collect_memorydata(time: int, frequency: int, mode="name", number=5):
         else:
             raise ValueError("mode {} not supported.".format(mode))
 
-        datapoints[_time] = {}
+        datapoints[current_time] = {}
 
         index = 0
         for line in out.split("\n"):
@@ -46,24 +58,40 @@ def collect_memorydata(time: int, frequency: int, mode="name", number=5):
             label = match.group("label")
             memory = int(match.group("memory"))
 
-            if label in datapoints[_time]:
-                memory += int(datapoints[_time][label])
+            if label in datapoints[current_time]:
+                memory += int(datapoints[current_time][label])
             else:
                 index += 1
-                if index >= number:
-                    if "other" not in datapoints[_time]:
-                        datapoints[_time]["other"] = 0
 
-                    datapoints[_time]["other"] = datapoints[_time]["other"] +\
-                                                 memory
+                if index >= number:
+                    if "other" not in datapoints[current_time]:
+                        datapoints[current_time]["other"] = 0
+
+                    datapoints[current_time]["other"] = \
+                        float(int(datapoints[current_time]["other"] +
+                                  memory/1024))
                     continue
 
-            datapoints[_time][label] = memory
+                labels.add(label)
 
-        for lab in datapoints[_time]:
-            mem = datapoints[_time][lab]
-            print("{},{},{}".format(_time, mem, lab))
+            datapoints[current_time][label] = float(int(memory/1024))
+            
+        # Update the clock
+        current_time = time.monotonic() - start_time
+
+    for key in datapoints:
+        labels_at_key = set(list(datapoints[key].keys()))
+        if labels_at_key != labels:
+            for _label in labels - labels_at_key:
+                datapoints[key][_label] = 0
+            for lab in datapoints[key]:
+                mem = datapoints[key][lab]
+                yield datatypes.Datapoint(key, mem, lab)
+
+# def _insert_datapoint(self, time, label, memory):
+    # self.datapoints[time][label] = float(int(memory/1024))
 
 
 if __name__ == "__main__":
-    collect_memorydata(10, 1, "name", 5)
+    for data in collect_memory_data(5, "name", 10):
+        print(data)
