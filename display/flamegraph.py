e8d91acb2c23bd1cdb7c7822feb3b3a40e2efa33
AndreiDiaconu1 <acdiaconu98@gmail.com>
2018-08-13 10:58:53 +0100

    Adding classes for the display module

--- display/flamegraph.py
+++ display/flamegraph.py
@@ -3,12 +3,16 @@
 # June-July 2018 - Franz Nowak, Hrutvik Kanabar
 # -------------------------------------------------------------
 """
-Interacts with the flamegraph tool.
+Class that interacts with the flamegraph tool.
 
-Issues a command to start the necessary scripts in Brendan Gregg's
-Flamegraph tool.
+Implements the GenericDiaplay interface to display an image of a flamegraph.
 
 """
+
+__all__ = (
+    "Flamegraph"
+)
+
 import os
 import subprocess
 import collections
@@ -19,6 +23,7 @@ from common import (
     util
 )
 from common.datatypes import StackData
+from display.generic_display import GenericDisplay
 
 logger = logging.getLogger(__name__)
 logger.debug('Entered module: {}'.format(__name__))
@@ -29,60 +34,79 @@ DISPLAY_DIR = str(os.path.dirname(os.path.dirname(os.path.realpath(
 FLAMEGRAPH_DIR = DISPLAY_DIR + "util/flamegraph/flamegraph.pl"
 
 
-@util.log(logger)
-def read(in_filename):
-    """Read stack events from a file in standard format."""
-    with open(in_filename, "r") as file:
-        # Skip first line, header
-        file.readline()
+class Flamegraph(GenericDisplay):
+    def __init__(self, *args):
+        """
 
-        for line in file.readlines():
-            yield StackData.from_string(line)
+        :param args[0] (in_filename):
+            The name of the [Stack] file to read data from
+        :param args[1] (out_filename):
+            The name of the image file that will be created.
+        :param args[2] (colouring):
+            The colouring for the flamegraph as an argument string.
+            As defined by Brendan Gregg's script, to go in the
+            "--color=" option.
+        """
+        if len(args) != 3:
+            raise Exception("Invalid number of parameters for the Flamegraph "
+                            "class. The correct ones are, in this order, "
+                            "in_filename, out_filename, colouring")
+        self.in_filename = args[0]
+        self.out_filename = args[1]
+        self.colouring = args[2]
 
+    @util.log(logger)
+    def _read(self):
+        """
+        Read stack events from a file in standard format.
 
-@util.log(logger)
-def make(stack_data, out_filename, colouring=None):
-    """
-    Uses Brendan Gregg's flamegraph tool to convert data to flamegraph.
+        """
+        with open(self.in_filename, "r") as inp:
+            # Skip first line, header
+            inp.readline()
 
-    :param stack_data:
-        Generator for :class:`StackData` objects.
-    :param out_filename:
-        The name of the image file that will be created.
-    :param colouring:
-        The colouring for the flamegraph as an argument string.
-        As defined by Brendan Gregg's script, to go in the
-        "--color=" option.
+            for line in inp.readlines():
+                yield StackData.from_string(line)
 
-    """
-    temp_file = str(file.TempFileName())
-    counts = collections.Counter()
-    for stack in stack_data:
-        new_counts = collections.Counter({stack.stack: stack.weight})
-        counts += new_counts
+    @util.log(logger)
+    def _make(self, stack_data):
+        """
+        Uses Brendan Gregg's flamegraph tool to convert data to flamegraph.
 
-    with open(temp_file, "w") as out:
-        for stack, count in counts.items():
-            out.write(";".join(stack) + " {}\n".format(count))
+        :param stack_data:
+            Generator for `StackData` objects.
 
-    with open(out_filename, "w") as out:
-        if colouring:
-            subprocess.Popen([FLAMEGRAPH_DIR, "--color=" + colouring,
-                              temp_file], stdout=out)
-        else:
-            subprocess.Popen([FLAMEGRAPH_DIR, temp_file], stdout=out)
+        """
+        temp_file = str(file.TempFileName())
+        counts = collections.Counter()
+        for stack in stack_data:
+            new_counts = collections.Counter({stack.stack: stack.weight})
+            counts += new_counts
 
+        with open(temp_file, "w") as out:
+            for stack, count in counts.items():
+                out.write(";".join(stack) + " {}\n".format(count))
 
-@util.log(logger)
-def show(image):
-    """
-    Uses firefox to display the flamegraph.
+        with open(self.out_filename, "w") as out:
+            if self.colouring:
+                subprocess.Popen([FLAMEGRAPH_DIR, "--color=" + self.colouring,
+                                  temp_file], stdout=out)
+            else:
+                subprocess.Popen([FLAMEGRAPH_DIR, temp_file], stdout=out)
 
-    :param image:
-        The name of the image file containing the flamegraph.
+    @util.log(logger)
+    @util.Override(GenericDisplay)
+    def show(self):
+        """
+        Creates the image and uses firefox to display the flamegraph.
 
-    """
-    username = os.environ['SUDO_USER'] #@@@ TODO test this
-    subprocess.call(["su", "-", "-c",  "firefox " + image,
-                     username])
+        """
+        # Generate data from the input file
+        stack_generator = self._read()
+        # Create a flamegraph svg based on the data
+        self._make(stack_generator)
+        # Open firefox
+        username = os.environ['SUDO_USER']
+        subprocess.call(["su", "-", "-c",  "firefox " + self.out_filename,
+                         username])
 
