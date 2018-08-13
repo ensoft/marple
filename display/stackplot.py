578b1f42dab2c538f4d562dd7ce969760f00b16c
franznowak <fgn24@cam.ac.uk>
2018-08-13 15:04:04 +0100

    Make stackplot into a class that accepts a filename to a csv file as input.

--- display/stackplot.py
+++ display/stackplot.py
@@ -1,16 +1,69 @@
+# -------------------------------------------------------------
+# stackplot.py - Creates a stackplot from labelled coordinate data.
+# June-July 2018 - Franz Nowak, Hrutvik
+# -------------------------------------------------------------
+"""
+# stackplot.py - Creates a stackplot from labelled coordinate data.
+
+Adds colours and legend and fills in between the lines.
+
+"""
+import logging
+
+__all__ = ("StackPlot", )
+
 import pandas as pd
 import numpy as np
 import io
 import matplotlib.pyplot as plt
 
+from common import util
 
-def show(csv):
-    df = pd.read_csv(io.StringIO(csv.strip()))
-    c = np.unique(df.C)
-    labels = ['{}'.format(_c) for _c in c]
-    args = [df[df.C == _c].B for _c in c]
-    plt.stackplot(np.unique(df.A), args, labels=labels)
-    plt.xlabel('A')
-    plt.ylabel('C')
-    plt.legend(loc='upper left')
-    plt.show()
+logger = logging.getLogger(__name__)
+
+
+class StackPlot:
+    """
+    The class representing stackplots.
+
+    Takes a filename with CSV formatted data as input.
+        To show the heat map, use :func:`StackPlot.show`.
+
+    """
+    @util.log(logger)
+    def __init__(self, filename):
+        """
+        Constructor, initialises the stackplot.
+
+        :param filename:
+            The name of the file containing the csv data.
+
+        """
+        with open(filename, "r", encoding="utf-8") as file_:
+            df = pd.read_csv(file_)
+        c = np.unique(df.C)
+        labels = ['{}'.format(_c) for _c in c]
+        args = [df[df.C == _c].B for _c in c]
+        plt.stackplot(np.unique(df.A), args, labels=labels)
+        plt.xlabel('time/s')
+        plt.ylabel('memory/mB')
+        plt.legend(loc='upper left')
+
+    @util.log(logger)
+    def show(self):
+        plt.show()
+
+
+if __name__ == "__main__":
+    filename = "/tmp/stackplot.plt"
+    with open(filename, "w") as file_:
+        file_.writelines(io.StringIO("""
+        A,B,C
+        0,0,a
+        0,0,b
+        1,1,a
+        1,1,b
+        """.strip()))
+
+    sp = StackPlot(filename)
+    sp.show()
