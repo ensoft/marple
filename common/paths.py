e8eff7af0e2f0c9adab067dca6bb2be8c4cfaaa4
Hrutvik Kanabar <hrutvikkanabar@yahoo.co.uk>
2018-08-23 12:40:34 +0100

    Fix testing import errors

--- common/paths.py
+++ common/paths.py
@@ -4,17 +4,24 @@
 # -------------------------------------------------------------
 
 """Stores standard paths to files"""
-__all__ = ["create_directories"]
+__all__ = (
+    'create_directories',
+    'LOG_DIR',
+    'TMP_DIR',
+    'VAR_DIR',
+    'MARPLE_DIR',
+    'OUT_DIR'
+)
 
 import os
-import main
 
 LOG_DIR = "/var/log/marple/"
 TMP_DIR = "/tmp/marple/"
 VAR_DIR = "/var/lib/"
 
-MARPLE_DIR = os.path.dirname(os.path.abspath(main.__file__))
+MARPLE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 OUT_DIR = os.getcwd() + "/marple_out/"
+
 
 def create_directories():
     if not os.path.isdir(LOG_DIR):
