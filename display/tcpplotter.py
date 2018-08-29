eb3f1afe50e662a395f630e3ae7cc8b00e71fc5c
AndreiDiaconu1 <acdiaconu98@gmail.com>
2018-08-29 19:08:39 +0100

    Changed the TCPPlotter drawing from matplotlib to pyqtgraph

--- display/tcpplotter.py
+++ display/tcpplotter.py
@@ -8,10 +8,12 @@ Display option that can display ipc/tcp data as a line graph
 
 """
 
-import matplotlib.pyplot as plt
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtWidgets
 import logging
 
 import typing
+import enum
 
 from common import data_io
 from display.interface import generic_display
@@ -23,6 +25,100 @@ _all__ = (
 
 )
 
+
+class _TCPPoint(typing.NamedTuple):
+    pid: str
+    comm: str
+    port: str
+    net_ns: str
+
+
+class _TCPMessage(typing.NamedTuple):
+    source: _TCPPoint
+    destination: _TCPPoint
+    time: str
+    type: str
+
+
+# TODO: Tooltip for the unused data
+# TODO: Add multiple colors to lines (search for Luke's answer on coloring
+# TODO: discrete segments on the Google Forum)
+class _TCPDDrawWidget(pg.GraphicsWindow):
+    source_brush = pg.mkBrush('w')
+    destination_brush = pg.mkBrush('w')
+    accept_color = pg.mkColor('g')
+    close_color = pg.mkColor('r')
+    connect_color = pg.mkColor('b')
+
+    def __init__(self, messages, comm_to_y_map, parent=None):
+        super().__init__(parent=parent)
+
+        self.mainLayout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.mainLayout)
+
+        self.messages = messages
+        self.comm_to_y_map = comm_to_y_map
+
+        self._draw_background()
+        self._draw_data()
+
+    def _draw_background(self):
+        comm_axis = pg.AxisItem(orientation='left')
+        comm_axis.setTicks([dict(enumerate(self.comm_to_y_map.keys())).items()])
+        self.plotter = self.addPlot(title="IPC (TCP Messages)",
+                                    axisItems={'left': comm_axis})
+
+        max_y = max(self.comm_to_y_map.values())
+        max_time = max([message.time for message in self.messages])
+        self.plotter.vb.setLimits(xMin=-1, xMax=max_time + 1, yMin=-1,
+                                  yMax=max_y + 1)
+        for y in range(0, max_y + 1):
+            self.plotter.plot([0, max_time], [y, y],
+                              pen=pg.mkPen(255, 255, 255, 32))
+
+    def _draw_data(self):
+        symbols = []
+        for _ in range(0, len(self.messages)):
+            symbols.extend(['s', 'o'])
+
+        symbol_brushes = []
+        for _ in range(0, len(self.messages)):
+            symbol_brushes.extend([self.source_brush,
+                                   self.destination_brush])
+
+        xs = []
+        ys = []
+        line_colors = []
+        for message in self.messages:
+            xs.extend([message.time, message.time])
+            ys.extend([self.comm_to_y_map[message.source.comm],
+                       self.comm_to_y_map[message.destination.comm]])
+            if message.type == 'A':
+                color = self.accept_color
+            elif message.type == 'C':
+                color = self.connect_color
+            else:
+                color = self.close_color
+            line_colors.append(color)
+
+        self.plotter.plot({'x': xs, 'y': ys}, symbol=symbols,
+                          pen='b', symbolBrush=symbol_brushes,
+                          connect="pairs")
+
+    def set_lines(self, x, y):
+        symbols = []
+        for i in range(0, int(len(x)/2)):
+            symbols.extend(['s', 'o'])
+
+        colors = []
+        brush_sour = pg.mkBrush('g')
+        brush_dest = pg.mkBrush('r')
+        for i in range(0, int(len(x)/2)):
+            colors.extend([brush_sour, brush_dest])
+        self.plotDataLines.setData({'x':x, 'y':y}, symbol=symbols,
+                                   pen='r', symbolBrush=colors)
+
+
 class TCPPlotter(generic_display.GenericDisplay):
     """
     Class that displays tcp data as a custom line graph"
@@ -33,13 +129,13 @@ class TCPPlotter(generic_display.GenericDisplay):
 
     _DEFAULT_OPTIONS = DisplayOptions()
 
-    def __init__(self, data,
+    def __init__(self, data_gen,
                  data_options=data_io.StackData.DEFAULT_OPTIONS,
                  display_options=_DEFAULT_OPTIONS):
         """
         Initializes the class
 
-        :param data:
+        :param data_gen:
             A generator that returns the lines for the section we want to
             display as a flamegraph
         :param data_options: object of the class specified in each of the `Data`
@@ -48,23 +144,19 @@ class TCPPlotter(generic_display.GenericDisplay):
         :param display_options: display related options that are meant to make
                                 the display option more customizable
         """
-        self.data = data
+        # Initialise superclass
+        super().__init__(data_options, display_options)
 
-    def _plot_draw(self):
-        """
-        Helper function that draws the points and lines in the plotter
+        self.data = data_gen
 
-        It first draws the timeline for each process and its label.
-        Then we draw each source point and destination point and the line that
-        connects them, coloured and labeled correctly
-        """
+    def _get_data_and_mapping(self):
+        comm_to_y_map = {}
+        unique_ys = 0
+        messages = []
 
-        y_dict = {}
-        crt_y_location = 0
         for event_datum in self.data:
-            # We save all the data encoded in the event_datum
-            time = event_datum[0]
-            type = event_datum[1]
+            msgtime = event_datum[0]
+            msgtype = event_datum[1]
             source_pid = event_datum[2][0]
             source_comm = event_datum[2][1]
             source_port = event_datum[2][2]
@@ -73,59 +165,51 @@ class TCPPlotter(generic_display.GenericDisplay):
             dest_port = event_datum[2][5]
             net_ns = event_datum[2][6]
 
-            # If the current source_comm was not encountered before, add it
-            # to the dict with it's corresponding y location and add its axline
-            # crt_y_location represents the current y location where we can
-            # draw a new process; starts from 0 and goes up
-            if source_comm not in y_dict.keys():
-                y_dict[source_comm] = crt_y_location
-                plt.axhline(crt_y_location, color="darkgray")
-                crt_y_location += 1
+            source = _TCPPoint(source_pid, source_comm, source_port, net_ns)
+            dest = _TCPPoint(dest_pid, dest_comm, dest_port, net_ns)
+            message = _TCPMessage(source, dest, msgtime, msgtype)
 
-            # Same but now for dest_comm
-            if dest_comm not in y_dict.keys():
-                y_dict[dest_comm] = crt_y_location
-                plt.axhline(crt_y_location, color="darkgray")
-                crt_y_location += 1
+            if source.comm not in comm_to_y_map.keys():
+                comm_to_y_map[source.comm] = unique_ys
+                unique_ys += 1
 
-            if type == 'A':
-                color = 'g'
-            elif type == 'C':
-                color = 'b'
-            else:
-                color = 'r'
-            plt.plot([time], [y_dict[source_comm]], color='black', marker="$S$")
-            plt.plot([time], [y_dict[dest_comm]], color='black', marker="$D$")
-            plt.plot([time, time], [y_dict[source_comm], y_dict[dest_comm]],
-                     '{}-'.format(color), linewidth=1.0)
+            if dest.comm not in comm_to_y_map.keys():
+                comm_to_y_map[dest.comm] = unique_ys
+                unique_ys += 1
 
-        y_int = [idx for idx in range(0, len(y_dict.keys()))]
-        y_labels = [comm for comm in sorted(y_dict.keys())]
-        plt.yticks(y_int, y_labels)
+            messages.append(message)
+        return messages, comm_to_y_map
 
     def show(self):
         """
         Populates the graph and shows it in an interactive window
 
         """
-        self._plot_draw()
-        plt.show()
+        app = QtWidgets.QApplication([])
+
+        messages, comm_to_y_map = self._get_data_and_mapping()
+
+        renderer = _TCPDDrawWidget(messages, comm_to_y_map)
+        pg.setConfigOptions(antialias=False)
+        renderer.resize(800, 600)
+        renderer.show()
+        renderer.raise_()
+        app.exec_()
 
 
-# TODO: https://bastibe.de/2013-05-30-speeding-up-matplotlib.html
-# TODO: Add new
-# import random
-#
-# data = []
-# types = ['A', 'X', 'C']
-# for i in range(1, int(random.random() * 100)):
-#     time = random.random() * 100
-#     s = int(random.random() * 100)
-#     d = int(random.random() * 100)
-#     data.append(data_io.EventDatum(time=time,
-#                                    type=types[random.randrange(0, 3)],
-#                                    specific_datum=(
-#         None, 'process' + str(s), None, None, 'process' + str(d), None, None)))
-#
-# plot = TCPPlotter(data)
-# plot.show()
+import random
+
+data = []
+types = ['A', 'X', 'C']
+
+for i in range(1, int(random.random() * 100)):
+    time = random.random() * 100
+    s = int(random.random() * 100)
+    d = int(random.random() * 100)
+    data.append(data_io.EventDatum(time=time,
+                                   type=types[random.randrange(0, 3)],
+                                   specific_datum=(
+        None, 'process' + str(s), None, None, 'process' + str(d), None, None)))
+
+plot = TCPPlotter(data)
+plot.show()
